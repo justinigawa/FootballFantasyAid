613e2e3fd1dc830b9402dc11ef1d5d402863cba2
Justin Igawa <J_Igawa@dello780-ls.qrshq.local>
2016-11-26 13:00:40 -0800

    added footer

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,14 +1,19 @@
 import React, {Component} from 'react'
 import Header from '../components/Header'
+import Footer from '../components/Footer'
+import Body from '../components/Body'
 
 class Login extends Component {
 
     render() {
         return (
             <div>
-                <div><Header /></div>
-                <div>This is the login page</div>
-                <div>Login</div>
+                <div><Header/></div>
+                <div>
+                    <div>This is the login page</div>
+                    <div><Body/></div>
+                </div>
+                <div><Footer/></div>
             </div>
         )
     }
