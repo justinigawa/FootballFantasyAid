0e7f4f92384883d9149286778fd8a5e5b1f68509
Justin Igawa <J_Igawa@justins-air.attlocal.net>
2016-11-25 16:10:35 -0800

    changed body component, took out divs that aren't needed

--- src/components/Body.js
+++ src/components/Body.js
@@ -5,9 +5,7 @@ const Body = props => (
     <div className="container hero">
         <div className="row">
             <div className="col-md-8 col-md-offset-2">
-                <h1 className="text-center">Fantasy Aid</h1>
                 <div className="embed-responsive embed-responsive-16by9">
-                    <iframe className="embed-responsive-item"></iframe>
                 </div>
             </div>
         </div>
