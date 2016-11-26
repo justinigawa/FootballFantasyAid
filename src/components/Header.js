a2a0c4fb18435ad97de06b2dde4a30cbd5d91ad6
Justin Igawa <J_Igawa@dello780-ls.qrshq.local>
2016-11-26 12:59:48 -0800

    added footer

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,20 +5,8 @@ const Header = props => (
     <div className="header-dark">
         <nav className="navbar navbar-default navigation-clean-search">
             <div className="container">
-                <div className="navbar-header"><a className="navbar-brand navbar-link" href="/">Fantasy Aid</a>
-                    <button className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navcol-1"><span className="sr-only">Toggle navigation</span><span className="icon-bar"></span><span className="icon-bar"></span><span className="icon-bar"></span></button>
-                </div>
+                <div className="navbar-header"><a className="navbar-brand navbar-link" href="/">Fantasy Aid</a></div>
                 <div className="collapse navbar-collapse" id="navcol-1">
-                    <ul className="nav navbar-nav">
-                        <li className="dropdown"><a className="dropdown-toggle" data-toggle="dropdown" aria-expanded="false" href="#">Dropdown <span className="caret"></span></a>
-                            <ul className="dropdown-menu" role="menu">
-                                <li role="presentation"><a href="../Quarterback">Quarterbacks</a></li>
-                                <li role="presentation"><a href="../Runningback">Runningbacks</a></li>
-                                <li role="presentation"><a href="../TightEnd">Tight Ends</a></li>
-                                <li role="presentation"><a href="../WideReciever">Wide Recievers</a></li>
-                            </ul>
-                        </li>
-                    </ul>
                     <div className="navbar-text navbar-right">
                         <a className="navbar-link-li" href="../Login">Log In</a>
                         <a className="btn btn-default action-button" role="button" href="../Signup">Sign Up</a>
